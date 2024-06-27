31590178c7974eb919d37222bfb729aa969f1419
qifan.hu <qifan.hu@harman.com>
2024-06-27 18:20:22 +0800

    Neo config

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
-require "core"
-require "plugin-loader"
+require('qifan/core')
+require('qifan/lazy')
+
+
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=4 sts=4 sw=4 et
+
+
