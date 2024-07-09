6f8f056d030ef60d1e894e9b375465af43dfd3e5
huqifan <597909123@qq.com>
2024-07-09 23:35:13 +0800

    Update

--- lua/qifan/plugins/colorscheme.lua
+++ lua/qifan/plugins/colorscheme.lua
@@ -2,9 +2,7 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
-	-- opts = {
-	-- 	flavour = "frappe", -- latte, frappe, macchiato, mocha
-	-- },
+
 	config = function()
 		require("catppuccin").setup({
 			flavour = "mocha", -- latte, frappe, macchiato, mocha
