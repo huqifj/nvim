b23713fcce0c13eca9303d2951d307b13fad6a2f
qifan.hu <597909123@qq.com>
2024-07-18 15:14:31 +0800

    Support C/CPP debugging using vscode-tools

--- lua/user/plugins/dap.lua
+++ lua/user/plugins/dap.lua
@@ -14,23 +14,49 @@ return {
 		config = function()
 			local dap = require("dap")
 
-			dap.adapters.gdb = {
+			dap.adapters.cppdbg = {
+				id = "cppdbg",
 				type = "executable",
-				executable = {
-					command = vim.fn.exepath("gdb"),
-					args = { "-i", "dap" },
+				command = "D:/Portable/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7",
+				options = {
+					detached = false,
 				},
 			}
-			dap.configurations.c = {
-				name = "Launch file",
-				type = "gdb",
-				request = "launch",
-				gdbpath = function()
-					-- return "/usr/local/bin/gdb"
-					return "C:/Users/Qifan.Hu/scoop/apps/gdb/14.1/bin"
-				end,
-				cwd = "${workspaceFolder}",
+
+			dap.configurations.cpp = {
+				{
+					name = "Launch file",
+					type = "cppdbg",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopAtEntry = true,
+					setupCommands = {
+						{
+							text = "-enable-pretty-printing",
+							description = "enable pretty printing",
+							ignoreFailures = false,
+						},
+					},
+				},
+				{
+					name = "Attach to gdbserver :1234",
+					type = "cppdbg",
+					request = "launch",
+					MIMode = "gdb",
+					miDebuggerServerAddress = "localhost:1234",
+					miDebuggerPath = "/usr/bin/gdb",
+					cwd = "${workspaceFolder}",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+				},
 			}
+
+			dap.configurations.c = dap.configurations.cpp
+			dap.configurations.rust = dap.configurations.cpp
 		end,
 	},
 	{
