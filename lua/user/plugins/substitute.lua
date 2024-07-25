45ea3ed467e738d6f6f8236f3ae45ee39a739c88
qifan.hu <597909123@qq.com>
2024-07-25 11:21:49 +0800

    Using grub-far instead of nvim-spectre

--- lua/user/plugins/substitute.lua
+++ lua/user/plugins/substitute.lua
@@ -1,14 +1,45 @@
 return {
-	"gbprod/substitute.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local substitute = require("substitute")
+	{
+		"gbprod/substitute.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local substitute = require("substitute")
 
-		substitute.setup()
+			substitute.setup()
 
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
+			-- set keymaps
+			local keymap = vim.keymap -- for conciseness
 
-		keymap.set("n", "<leader>re", substitute.operator, { desc = "Substitute with motion" })
-	end,
+			keymap.set("n", "<leader>re", substitute.operator, { desc = "Substitute with motion" })
+		end,
+	},
+	{
+		"MagicDuck/grug-far.nvim",
+		config = function()
+			require("grug-far").setup({
+				wrap = false,
+				keymaps = {
+					replace = { n = "<localleader>r" },
+					qflist = { n = "<localleader>q" },
+					syncLocations = { n = "<localleader>s" },
+					syncLine = { n = "<localleader>l" },
+					close = { n = "<localleader>c" },
+					historyOpen = { n = "<localleader>t" },
+					historyAdd = { n = "<localleader>a" },
+					refresh = { n = "<localleader>f" },
+					openLocation = { n = "<localleader>o" },
+					gotoLocation = { n = "<enter>" },
+					pickHistoryEntry = { n = "<enter>" },
+					abort = { n = "<localleader>b" },
+					help = { n = "g?" },
+					toggleShowRgCommand = { n = "<localleader>p" },
+				},
+			})
+		end,
+
+
+		vim.keymap.set("n", "<leader>S", '<cmd>GrugFar<CR>', {
+			desc = "GrugFar",
+		}),
+	},
 }
