31ac2015d63b2865e7d98fe29ac760ca0276970b
qifan.hu <597909123@qq.com>
2024-08-16 17:32:45 +0800

    Update markdown plugin configuration

--- lua/user/plugins/markdown.lua
+++ lua/user/plugins/markdown.lua
@@ -1,7 +1,38 @@
 return {
-	"ellisonleao/glow.nvim",
-	config = function()
-		require("glow").setup()
-	end,
-	cmd = "Glow",
+	{
+		"ellisonleao/glow.nvim",
+		config = function()
+			require("glow").setup()
+		end,
+		cmd = "Glow",
+	},
+
+	-- install without yarn or npm
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+
+		config = function()
+			local keymap = vim.keymap
+
+			keymap.set("n", "<leader>mko", "<cmd>MarkdownPreview<CR>", { desc = "MarkdownPreview" })
+			keymap.set("n", "<leader>mkc", "<cmd>MarkdownPreviewStop<CR>", { desc = "MarkdownPreviewStop" })
+			keymap.set("n", "<leader>mkt", "<cmd>MarkdownPreviewToggle<CR>", { desc = "MarkdownPreviewToggle" })
+		end,
+	},
+
+	-- -- install with yarn or npm
+	-- {
+	-- 	"iamcco/markdown-preview.nvim",
+	-- 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	-- 	build = "cd app && yarn install",
+	-- 	init = function()
+	-- 		vim.g.mkdp_filetypes = { "markdown" }
+	-- 	end,
+	-- 	ft = { "markdown" },
+	-- },
 }
