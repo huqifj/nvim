a11dde50cf52020e353b3eee6551268c9a0a7624
Qifan <597909123@qq.com>
2025-05-26 13:33:42 +0800

    修复拼写错误

--- lua/user/core/keymaps.lua
+++ lua/user/core/keymaps.lua
@@ -19,7 +19,7 @@ keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window"
 keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
--- WIndow management
+-- Window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
