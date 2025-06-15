ae5cddc8bcd4e619e51856e79b8501f306ee62c3
huqifan <597909123@qq.com>
2025-06-15 14:43:10 +0800

    temp

--- lua/qifan/plugins.bak/just-for-fun.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"eandrju/cellular-automaton.nvim",
-	event = "VeryLazy",
-
-	config = function()
-		-- require("cellular-automaton").setup({})
-		vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
-	end,
-}
--- lua/qifan/plugins.bak/which-key.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'folke/which-key.nvim',
-    event = 'VeryLazy',
-    init = function()
-    end,
-    opts = {
-    },
-}
