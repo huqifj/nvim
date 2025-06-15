94a17e01df5d227777baa9af20393422e9d0c6fb
huqifan <597909123@qq.com>
2025-06-15 23:45:56 +0800

    temp

--- init.lua
+++ init.lua
@@ -3,14 +3,3 @@ require('core.lazy')
 require('core.lsp')
 require('config.keymaps')
 require('config.autocmds')
-
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- 设置本文件缩进格式
--- tabstop: 一个 tab 显示为几个空格
--- softtabstop: 进行编辑操作时（例如 insert 和 backspace），tab 计算为多少个空格
--- shiftwidth: 每一级缩进的空格数量
--- expandtab: 按下 tab 时转换为空格
--- vim: tabstop=4 softtabstop=4 shiftwidth=4 expandtab
-
-
