0afbd3c8cc5c79ae727534e550e8bb535b6052ee
huqifan <597909123@qq.com>
2025-07-15 10:37:00 +0800

    wefwef

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -31,7 +31,11 @@ require("lazy").setup({
 	-- colorscheme that will be used when installing plugins.
 	install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
-	checker = { enabled = true },
+	checker = {
+		enabled = true,
+		frequency = 3600 * 24, -- check for updates every day
+		notify = false,
+	},
 	change_detection = {
 		notify = false,
 	},
