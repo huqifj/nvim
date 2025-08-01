fb418fd1772250776b4ce26d385b3e5f378e642e
huqifan <597909123@qq.com>
2025-08-01 17:19:15 +0800

    Add keymap to move tab left or right

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,10 @@
 return {
-    'akinsho/bufferline.nvim',
-    version = "*",
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    opts = {}
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		require("bufferline").setup({})
+		vim.keymap.set("n", "<leader>[b", "<cmd>BufferLineMovePrev<CR>")
+		vim.keymap.set("n", "<leader>]b", "<cmd>BufferLineMoveNext<CR>")
+	end,
 }
