0a1b77b71333d89f9389d5449af583c931241483
huqifan <597909123@qq.com>
2025-08-05 15:59:16 +0800

    im-select

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -1,9 +1,13 @@
-return {
-	"keaising/im-select.nvim",
-	config = function()
-		require("im_select").setup({
-			default_im_select = "英文模式",
-			default_command = "im-select-mspy.x64.exe",
-		})
-	end,
-}
+if vim.fn.has("win32") == 1 then
+	return {
+		"keaising/im-select.nvim",
+		config = function()
+			require("im_select").setup({
+				default_im_select = "英文模式",
+				default_command = "im-select-mspy.x64.exe",
+			})
+		end,
+	}
+else
+	return {}
+end
