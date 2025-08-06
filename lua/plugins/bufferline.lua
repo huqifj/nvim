df1b61085121c092e8aeb1ec44c1de58dd09518a
huqifan <597909123@qq.com>
2025-08-06 15:30:07 +0800

    update

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,7 +4,7 @@ return {
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
 		require("bufferline").setup({})
-		vim.keymap.set("n", "<leader>[b", "<cmd>BufferLineMovePrev<CR>")
-		vim.keymap.set("n", "<leader>]b", "<cmd>BufferLineMoveNext<CR>")
+		vim.keymap.set("n", "<leader>[b", "<cmd>BufferLineCyclePrev<CR>")
+		vim.keymap.set("n", "<leader>]b", "<cmd>BufferLineCycleNext<CR>")
 	end,
 }
