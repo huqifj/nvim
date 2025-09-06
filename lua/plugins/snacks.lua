8cba6e092c38b1d0c85a18779af0ece6f96ef51c
huqifan <597909123@qq.com>
2025-09-06 14:39:12 +0800

    update

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -72,6 +72,8 @@ return {
 		{ "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
 		{ "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
 		{ "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
+
+		-- Search
 		{ "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
 		{ "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
 
